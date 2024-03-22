18fb0b5112f491cdc5a7ffa6edc111ed6a6c2991
YuvrajPraveenSingh <yuv00191@outlook.com>
2024-03-22 19:26:20 +0530

    Update backend configuration and variable names

--- provider.tf
+++ provider.tf
@@ -1,5 +1,10 @@
 terraform {
   backend "s3" {
+    bucket = var.backend_bucket_name
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "terraform-state-lock"
+    encrypt = true
     
   }
   required_providers {
--- variables.tf
+++ variables.tf
@@ -1,15 +1,10 @@
-variable "bucket_name" {
+variable "backend_bucket_name" {
     type = string
     default = "devops-terraform-state-2024"
     description = "The name of the S3 bucket to create"
   
 }
-variable "DynamoDB_name" {
-    type = string
-    default = "terraform-state-lock"
-    description = "The name of the DynamoDB table to create"
-  
-}
+
 variable "region" {
     type = string
     default = "us-east-1"
