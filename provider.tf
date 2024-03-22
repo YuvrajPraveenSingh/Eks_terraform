2eeb23f8f36cd557832550d4aeca19e0c440c16b
YuvrajPraveenSingh <yuv00191@outlook.com>
2024-03-22 18:29:26 +0530

    Refactor code for improved performance and readability

---
+++ provider.tf
@@ -0,0 +1,15 @@
+terraform {
+  backend "s3" {
+    
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+provider "aws" {
+  region = var.region
+}
+
